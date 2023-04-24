b7b4abf6e69ab9c1ee8156349603841f745ecbf4
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-04-24 11:06:36 -0700

    ok ig

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+var htmlshit  = document.getElementsByTagName('html');
+ htmlshit.innerHTML += '<div id="mydiv"><iframe src="https://crazygird-organization.github.io/mercury-clone/index/"></div><script src= "https://crazygird-organization.github.io/mercury-clone/drag.js"></script>';
